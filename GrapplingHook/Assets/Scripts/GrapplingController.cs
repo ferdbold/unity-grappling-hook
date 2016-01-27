5e10f7c04567ed1e2e311e7b31a56549ca3cd786
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-01-27 16:35:05 -0500

    Basic controller and hook

---
+++ GrapplingHook/Assets/Scripts/GrapplingController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+namespace Simoncouche.Prototypes {
+
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class GrapplingController : MonoBehaviour {
+
+		[Tooltip("Reference to the grappling hook prefab")]
+		[SerializeField]
+		private GrapplingHook _grapplingHookPrefab;
+
+		private GrapplingHook _grapplingHookObj;
+
+		public void Update () {
+			if (Input.GetButtonDown("Fire") && !isGrapplingHookActive) {
+				Fire();
+			}
+		}
+
+		private void Fire() {
+			_grapplingHookObj = (GrapplingHook)GameObject.Instantiate(_grapplingHookPrefab, transform.position, transform.rotation);
+			_grapplingHookObj.onHit.AddListener(RecallGrapplingHook);
+		}
+
+		public void RecallGrapplingHook() {
+			GameObject.Destroy(_grapplingHookObj.gameObject);
+			_grapplingHookObj = null;
+		}
+
+		private bool isGrapplingHookActive {
+			get {
+				return _grapplingHookObj != null;
+			}
+		}
+	}
+}
--- GrapplingHook/Assets/Scripts/GrapplingHook.cs
+++ GrapplingHook/Assets/Scripts/GrapplingHook.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
 
-public class GrapplingHook : MonoBehaviour {
+namespace Simoncouche.Prototypes {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class GrapplingHook : MonoBehaviour {
+
+		private Rigidbody2D _rigidbody;
+
+		[SerializeField]
+		private UnityEvent _onHit;
+
+		public void Awake() {
+			_rigidbody = GetComponent<Rigidbody2D>();
+			_rigidbody.AddForce(new Vector2(1, 1), ForceMode2D.Impulse);
+		}
+
+		public void Update() {
+			if (Input.GetButtonDown("Fire")) {
+				onHit.Invoke();
+			}
+		}
+
+		public UnityEvent onHit { get { return _onHit; } }
 	}
 }
